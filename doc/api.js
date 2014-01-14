eb676695a86dcee5adbf1b95ecd3695718f1ea72
casatt <matthieuholzer@gmx.de>
2014-01-14 01:04:26 +0100

    Documentation added

---
+++ doc/api.js
@@ -0,0 +1,109 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Computation",
+        "Crypto",
+        "Database",
+        "FileSystem",
+        "GeoLocation",
+        "Job",
+        "Jobs",
+        "Logger",
+        "MuskepeerClient",
+        "MuskepeerModule",
+        "Network",
+        "Node",
+        "Nodes",
+        "Peer",
+        "Peers",
+        "Project",
+        "Result",
+        "Service",
+        "Settings",
+        "Storage",
+        "Uuid",
+        "Worker",
+        "Workers"
+    ],
+    "modules": [
+        "Computation",
+        "Crypto",
+        "Database",
+        "FileSystem",
+        "GeoLocation",
+        "Jobs",
+        "Logger",
+        "MuskepeerClient",
+        "Network",
+        "Project",
+        "Settings",
+        "Storage",
+        "Uuid",
+        "Workers"
+    ],
+    "allModules": [
+        {
+            "displayName": "Computation",
+            "name": "Computation"
+        },
+        {
+            "displayName": "Crypto",
+            "name": "Crypto"
+        },
+        {
+            "displayName": "Database",
+            "name": "Database",
+            "description": "Database"
+        },
+        {
+            "displayName": "FileSystem",
+            "name": "FileSystem",
+            "description": "FileSystem"
+        },
+        {
+            "displayName": "GeoLocation",
+            "name": "GeoLocation"
+        },
+        {
+            "displayName": "Jobs",
+            "name": "Jobs"
+        },
+        {
+            "displayName": "Logger",
+            "name": "Logger"
+        },
+        {
+            "displayName": "MuskepeerClient",
+            "name": "MuskepeerClient"
+        },
+        {
+            "displayName": "Network",
+            "name": "Network"
+        },
+        {
+            "displayName": "Project",
+            "name": "Project",
+            "description": "Represents the current project"
+        },
+        {
+            "displayName": "Settings",
+            "name": "Settings",
+            "description": "Store for User-Settings which will be saved/read from\nlocalStorage"
+        },
+        {
+            "displayName": "Storage",
+            "name": "Storage",
+            "description": "Storage"
+        },
+        {
+            "displayName": "Uuid",
+            "name": "Uuid",
+            "description": "Module to generate and test UUIDs"
+        },
+        {
+            "displayName": "Workers",
+            "name": "Workers"
+        }
+    ]
+} };
+});
